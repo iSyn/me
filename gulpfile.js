1f68fe10dedfd9e3d89928d38ed8228dafba41a5
Synclair Wang <synclairwang11@yahoo.com>
2018-06-17 22:11:41 -0400

    pushing up files

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+let gulp = require('gulp')
+let sass = require('gulp-sass')
+let browserSync = require('browser-sync').create()
+
+gulp.task('browserSync', () => {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            // directory: true
+        }
+    })
+})
+
+gulp.task('sass', () => { // create a gulp task called sass
+    return gulp.src('app/scss/**/*.scss')  // tells gulp what files are needed
+        .pipe(sass()) // sends the files through gulp sass
+        .on('error', (err) => console.log('err', err)) // prevent gulp watch from crashing
+        .pipe(gulp.dest('app/css')) // destination of files
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+// Gulp watch syntax
+// gulp.watch('files-to-watch', ['tasks', 'to', 'run']); 
+
+gulp.task('watch', ['browserSync'], () => {
+    gulp.watch('app/scss/**/*.scss', ['sass'])
+    gulp.watch('./*.html', browserSync.reload); 
+    gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
